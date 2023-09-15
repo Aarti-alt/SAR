d7417bb3b3464b42588cdb68026e033f992f1498
Arti Chauhan <57387540+Aarti-alt@users.noreply.github.com>
2023-09-15 14:32:17 +0000

    new

--- app3.py
+++ app3.py
@@ -523,8 +523,8 @@ st.markdown("""
 }
 
 .css-1544g2n {
-    padding: 3rem 1rem 1.5rem;
-    padding-top: 3rem;
+    padding: 2rem 1rem 1.5rem;
+    padding-top: 2rem;
     padding-right: 1.5rem;
     padding-bottom: 1.5rem;
     padding-left: 1rem;
